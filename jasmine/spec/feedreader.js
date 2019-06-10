dfba9349d8f52ae26d21aaccce1b6734f6b15714
call900913 <call900913@icloud.com>
2019-06-10 10:46:06 -0400

    Write tests for the RSS-feed-reader app.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,32 +50,80 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+      var body = document.querySelector('body');
+      var bool3an = false;
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+      /* TODO: Write a test that ensures the menu element is
+       * hidden by default. You'll have to analyze the HTML and
+       * the CSS to determine how we're performing the
+       * hiding/showing of the menu element.
+       */
+      it('is hidden by default', function() {
+        expect(body.className).toBe('menu-hidden');
+      });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+      /* TODO: Write a test that ensures the menu changes
+       * visibility when the menu icon is clicked. This test
+       * should have two expectations: does the menu display when
+       * clicked and does it hide when clicked again.
+       */
+       it('changes visibility when clicked', function() {
+         /* So what we are doing here is simulating the click without actually
+          * firing a click event. So the route I've taken to do that is to fire
+          * what happens after the click event fires: the class toggle.
           */
+         body.classList.toggle('menu-hidden');
+         expect(body.className).toBe('');
+         body.classList.toggle('menu-hidden');
+         expect(body.className).toBe('menu-hidden');
+       });
+
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+      beforeEach(function(done) {
+        loadFeed(0, function() {
+          done();
+        });
+      });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      it('should have non-empty feed container', function(done) {
+        var list = document.querySelector('.feed').querySelectorAll('.entry-link');
+        expect(list.length).not.toBe(0);
+        done();
+      });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+      var list1 = document.querySelector('.feed').querySelectorAll('.entry-link');
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+      beforeEach(function(done) {
+        loadFeed(1, function() {
+          done();
+        });
+      });
+
+       it('should have different content', function(done) {
+         var list2 = document.querySelector('.feed').querySelectorAll('.entry-link');
+         expect(list2).not.toBe(list1);
+         done();
+       });
+    });
+
+
 }());
