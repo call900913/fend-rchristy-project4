e4f85c86824a24af5de77004cc964327db7ee51f
call900913 <call900913@icloud.com>
2019-06-11 14:46:41 -0400

    Add nested callbacks for testing asynchronous functions.

--- js/app.js
+++ js/app.js
@@ -128,7 +128,6 @@ $(function() {
      * on the body to perform the hiding/showing of our menu.
      */
     menuIcon.on('click', function() {
-
         $('body').toggleClass('menu-hidden');
     });
 }());
